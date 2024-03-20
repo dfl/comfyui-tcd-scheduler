24f60e499e6f876fd3505577d0e38f3071aedec0
David Löwenfels <282+dfl@users.noreply.github.com>
2024-03-20 09:35:33 +0000

    code from laksjdjf

--- sampler_tcd.py
+++ sampler_tcd.py
@@ -1,42 +1,41 @@
 import comfy.samplers
-from comfy.k_diffusion.sampling import default_noise_sampler
+from comfy.k_diffusion.sampling import default_noise_sampler, to_d
 from tqdm.auto import trange, tqdm
 from itertools import product
 import torch
 from torch import nn
 
 @torch.no_grad()
-def sample_tcd(model, x, sigmas, extra_args=None, callback=None, disable=None, noise_sampler=None, eta=0.3):
-    s_in = x.new_ones([x.shape[0]])
+def sample_tcd(model, x, sigmas, extra_args=None, callback=None, disable=None, noise_sampler=None, eta=None):
+    extra_args = {} if extra_args is None else extra_args
     noise_sampler = default_noise_sampler(x) if noise_sampler is None else noise_sampler
-    steps = len(sigmas)-1
-    for i in trange(steps, disable=disable):  # Reverse diffusion steps
-        # Adjusted Timestep Calculation (Strategic Stochastic Sampling):
-        ii = int((1 - eta) * i)
-
-        s0 = sigmas[i]
-        s1 = sigmas[i + 1]
-
-        # Generate denoised estimate from the model
-        x = model(x, s0 * s_in, **extra_args)
-
-        # Callback for progress monitoring/logging
+    s_in = x.new_ones([x.shape[0]])
+    for i in trange(len(sigmas) - 1, disable=disable):
+        denoised = model(x, sigmas[i] * s_in, **extra_args)
         if callback is not None:
-            callback({'x': x, 'i': i, 'sigma': s0, 'denoised': x})
+            callback({'x': x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigmas[i], 'denoised': denoised})
 
-        if eta > 0 and s1 > 0:
-            # introduce stochasticity
-            noise = noise_sampler(s0, s1)
-            # sigma = std; beta = variance; alpha = 1.0 - beta = 1 - sigma**2
-            a0 = 1.0 - s0**2
-            a1 = 1.0 - s1**2
-            a0s = 1.0 - sigmas[ii]
-            # adjusted variance scaling
-            v = a0*a1 / a0s # cumulative alpha product rescaled by the adjusted timestep
-            deterministic = v.sqrt() * x
-            stochastic = (1 - v).sqrt() * noise
-            x = deterministic + stochastic
+        d = to_d(x, sigmas[i], denoised)
 
+        sigma_from = sigmas[i]
+        sigma_to = sigmas[i + 1]
+
+        t = model.inner_model.inner_model.model_sampling.timestep(sigma_from)
+        # down_t = torch.floor( (1 - eta) * t )
+        down_t = (1 - eta) * t
+        sigma_down = model.inner_model.inner_model.model_sampling.sigma(down_t)
+
+        if sigma_down > sigma_to:
+            sigma_down = sigma_to
+
+        sigma_up = (sigma_to ** 2 - sigma_down ** 2) ** 0.5
+        
+        # same as euler ancestral
+        d = to_d(x, sigma_from, denoised)
+        dt = sigma_down - sigma_from
+        x = x + d * dt
+        if sigma_to > 0:
+            x = x + noise_sampler(sigma_from, sigma_to) * sigma_up
     return x
 
 class TCDScheduler:
