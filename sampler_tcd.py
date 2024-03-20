0d940afe4efdb85785b4ce8a604bdfa90b647897
David Löwenfels <282+dfl@users.noreply.github.com>
2024-03-20 20:45:31 +0000

    add EulerA sampler for comparison

--- sampler_tcd.py
+++ sampler_tcd.py
@@ -5,11 +5,44 @@ from itertools import product
 import torch
 from torch import nn
 
-def sample_tcd_lcm
+@torch.no_grad()
+def sample_tcd_euler_a(model, x, sigmas, extra_args=None, callback=None, disable=None, noise_sampler=None, gamma=0.3):
+    # TCD sampling using modified Euler Ancestral sampler. by @laksjdjf
+    extra_args = {} if extra_args is None else extra_args
+    noise_sampler = default_noise_sampler(x) if noise_sampler is None else noise_sampler
+    s_in = x.new_ones([x.shape[0]])
+    for i in trange(len(sigmas) - 1, disable=disable):
+        denoised = model(x, sigmas[i] * s_in, **extra_args)
+        if callback is not None:
+            callback({'x': x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigmas[i], 'denoised': denoised})
+
+        d = to_d(x, sigmas[i], denoised)
+
+        sigma_from = sigmas[i]
+        sigma_to = sigmas[i + 1]
+
+        t = model.inner_model.inner_model.model_sampling.timestep(sigma_from)
+        down_t = (1 - gamma) * t
+        sigma_down = model.inner_model.inner_model.model_sampling.sigma(down_t)
+
+        if sigma_down > sigma_to:
+            sigma_down = sigma_to
+
+        sigma_up = (sigma_to ** 2 - sigma_down ** 2) ** 0.5
+        
+        # same as euler ancestral
+        d = to_d(x, sigma_from, denoised)
+        dt = sigma_down - sigma_from
+        x += d * dt
+
+        if sigma_to > 0 and gamma > 0:
+            x += noise_sampler(sigma_from, sigma_to) * sigma_up
+    return x
+
 
 @torch.no_grad()
 def sample_tcd(model, x, sigmas, extra_args=None, callback=None, disable=None, noise_sampler=None, gamma=0.3):
-    """TCD sampling with a DDPMSampler_step-like process."""
+    # TCD sampling using modified DDPM.
     extra_args = {} if extra_args is None else extra_args
     noise_sampler = default_noise_sampler(x) if noise_sampler is None else noise_sampler
     s_in = x.new_ones([x.shape[0]])
@@ -23,13 +56,14 @@ def sample_tcd(model, x, sigmas, extra_args=None, callback=None, disable=None, n
 
         # TCD offset, based on gamma, and conversion between sigma and timestep
         t = model.inner_model.inner_model.model_sampling.timestep(sigma_from)
-        t_s = (1 - gamma) * t 
-        sigma_down = model.inner_model.inner_model.model_sampling.sigma(t_s)
+        t_s = (1 - gamma) * t
+        sigma_to_s = model.inner_model.inner_model.model_sampling.sigma(t_s)
 
-        if sigma_down > sigma_to:
-            sigma_down = sigma_to
-        # if sigma_down < 0:
-        #     sigma_down = torch.tensor(1.0)
+        # if sigma_to_s > sigma_to:
+        #     sigma_to_s = sigma_to
+        # if sigma_to_s < 0:
+        #     sigma_to_s = torch.tensor(1.0)
+        print(f"sigma_from: {sigma_from}, sigma_to: {sigma_to}, sigma_to_s: {sigma_to_s}")
 
 
         # The following is equivalent to the comfy DDPM implementation
@@ -48,7 +82,7 @@ def sample_tcd(model, x, sigmas, extra_args=None, callback=None, disable=None, n
         # print(f"alpha: {alpha}")
 
 
-        # alpha_cumprod_down = 1 / ((sigma_down * sigma_down) + 1)  # _s
+        # alpha_cumprod_down = 1 / ((sigma_to_s * sigma_to_s) + 1)  # _s
         # alpha_d = (alpha_cumprod_prev / alpha_cumprod_down)
         # alpha2 = (alpha_cumprod / alpha_cumprod_down)
         # print(f"** alpha_cumprod_down: {alpha_cumprod_down}")
@@ -115,7 +149,24 @@ class SamplerTCD:
         sampler = comfy.samplers.KSAMPLER(sample_tcd, extra_options={"gamma": gamma})
         return (sampler, )
 
+class SamplerTCDEulerA:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+                     "gamma": ("FLOAT", {"default": 0.3, "min": 0, "max": 1.0, "step": 0.01, "round": False})
+                    }
+               }
+    RETURN_TYPES = ("SAMPLER",)
+    CATEGORY = "sampling/custom_sampling/samplers"
+
+    FUNCTION = "get_sampler"
+
+    def get_sampler(self, gamma):
+        sampler = comfy.samplers.KSAMPLER(sample_tcd_euler_a, extra_options={"gamma": gamma})
+        return (sampler, )
+
 NODE_CLASS_MAPPINGS = {
     "TCDScheduler": TCDScheduler,
     "SamplerTCD": SamplerTCD,
+    "SamplerTCD EulerA": SamplerTCDEulerA,
 }
