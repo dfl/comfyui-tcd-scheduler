95340f97e5258d52470d5c1d6ad58ca125d19468
David Löwenfels <282+dfl@users.noreply.github.com>
2024-03-20 10:57:40 +0000

    rename vars

--- sampler_tcd.py
+++ sampler_tcd.py
@@ -6,7 +6,7 @@ import torch
 from torch import nn
 
 @torch.no_grad()
-def sample_tcd(model, x, sigmas, extra_args=None, callback=None, disable=None, noise_sampler=None, eta=None):
+def sample_tcd(model, x, sigmas, extra_args=None, callback=None, disable=None, noise_sampler=None, gamma=0.3):
     extra_args = {} if extra_args is None else extra_args
     noise_sampler = default_noise_sampler(x) if noise_sampler is None else noise_sampler
     s_in = x.new_ones([x.shape[0]])
@@ -20,9 +20,8 @@ def sample_tcd(model, x, sigmas, extra_args=None, callback=None, disable=None, n
         sigma_from = sigmas[i]
         sigma_to = sigmas[i + 1]
 
-        t = model.inner_model.inner_model.model_sampling.timestep(sigma_from)
-        # down_t = torch.floor( (1 - eta) * t )
-        down_t = (1 - eta) * t
+        from_t = model.inner_model.inner_model.model_sampling.timestep(sigma_from)
+        down_t = (1-gamma) * from_t
         sigma_down = model.inner_model.inner_model.model_sampling.sigma(down_t)
 
         if sigma_down > sigma_to:
@@ -33,9 +32,9 @@ def sample_tcd(model, x, sigmas, extra_args=None, callback=None, disable=None, n
         # same as euler ancestral
         d = to_d(x, sigma_from, denoised)
         dt = sigma_down - sigma_from
-        x = x + d * dt
+        x += d * dt
         if sigma_to > 0:
-            x = x + noise_sampler(sigma_from, sigma_to) * sigma_up
+            x += noise_sampler(sigma_from, sigma_to) * sigma_up
     return x
 
 class TCDScheduler:
@@ -59,7 +58,7 @@ class SamplerTCD:
     @classmethod
     def INPUT_TYPES(s):
         return {"required": {
-                     "eta": ("FLOAT", {"default": 0.3, "min": 0, "max": 1.0, "step": 0.01, "round": False})
+                     "gamma": ("FLOAT", {"default": 0.3, "min": 0, "max": 1.0, "step": 0.01, "round": False})
                     }
                }
     RETURN_TYPES = ("SAMPLER",)
@@ -67,8 +66,8 @@ class SamplerTCD:
 
     FUNCTION = "get_sampler"
 
-    def get_sampler(self, eta):
-        sampler = comfy.samplers.KSAMPLER(sample_tcd, extra_options={"eta": eta})
+    def get_sampler(self, gamma):
+        sampler = comfy.samplers.KSAMPLER(sample_tcd, extra_options={"gamma": gamma})
         return (sampler, )
 
 NODE_CLASS_MAPPINGS = {
